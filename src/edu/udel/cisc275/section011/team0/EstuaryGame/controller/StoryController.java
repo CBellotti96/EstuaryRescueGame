359a7894fd52ecd9727d53d58f2a60f158e669e0
Emily McNeil <Emily@DESKTOP-L16AD2B>
2017-04-06 10:14:10 -0400

    +View, Controller packages

---
+++ src/edu/udel/cisc275/section011/team0/EstuaryGame/controller/StoryController.java
@@ -0,0 +1,13 @@
+package edu.udel.cisc275.section011.team0.EstuaryGame.controller;
+
+public class StoryController {
+
+	private StoryModel model;
+	private StoryView view;
+	
+	public void tick(){
+		return;
+	}
+	
+	
+}
