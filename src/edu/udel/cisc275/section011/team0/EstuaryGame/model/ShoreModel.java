afa3756b10fc9e5236d349a438e9f536aa543628
Ben Wiswell <bwiswell@udel.edu>
2017-04-06 09:56:34 -0400

    Adding shore model class

---
+++ src/edu/udel/cisc275/section011/team0/EstuaryGame/model/ShoreModel.java
@@ -0,0 +1,49 @@
+package edu.udel.cisc275.section011.team0.EstuaryGame.model;
+
+import java.util.HashMap;
+
+public class ShoreModel {
+	
+	private double cursorXPos;
+	private double cursorYPos;
+	private double shoreHealth;
+	private HashMap<ShoreItemType, Integer> inventory;
+	private ShoreGameMode gameMode;
+	
+	public double getCursorXPos () {
+		return this.cursorXPos;
+	}
+	
+	public double getCursorYPos () {
+		return this.cursorYPos;
+	}
+	
+	public double getShoreHealth () {
+		return this.shoreHealth;
+	}
+	
+	public HashMap<ShoreItemType, Integer> getInventory () {
+		return this.inventory;
+	}
+	
+	public ShoreGameMode getGameMode () {
+		return this.gameMode;
+	}
+	
+	public void setCursorXPos (double cursorXPos) {
+		this.cursorXPos = cursorXPos;
+	}
+	
+	public void setCursorYPos (double cursorYPos) {
+		this.cursorYPos = cursorYPos;
+	}
+	
+	public void setShoreHealth (double shoreHealth) {
+		this.shoreHealth = shoreHealth;
+	}
+	
+	public void setGameMode (ShoreGameMode gameMode) {
+		this.gameMode = gameMode;
+	}
+
+}
