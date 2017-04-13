0cb36e7ae654b99bee51318f95ae70f970ff55e7
Alexandra Hurst <ahurst@udel.edu>
2017-04-13 17:17:32 -0400

    Fixed crash due to MenuController not actually instantiating model and view

--- src/edu/udel/cisc275/section011/team0/EstuaryGame/Controller/MenuController.java
+++ src/edu/udel/cisc275/section011/team0/EstuaryGame/Controller/MenuController.java
@@ -4,10 +4,18 @@ import java.awt.event.MouseAdapter;
 
 import javax.swing.JComponent;
 
+import edu.udel.cisc275.section011.team0.EstuaryGame.Model.MenuModel;
+import edu.udel.cisc275.section011.team0.EstuaryGame.View.MenuView;
+
 public class MenuController extends MouseAdapter implements Controller {
 	
 	private MenuModel model;
 	private MenuView view;
+	
+	public MenuController() {
+		model = new MenuModel();
+		view = new MenuView();
+	}
 	
 	@Override
 	public void tick(){
