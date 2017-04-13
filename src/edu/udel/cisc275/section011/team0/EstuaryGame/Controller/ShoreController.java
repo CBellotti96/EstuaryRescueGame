89a15db4a354ec6194eecc9b7d6fc30a6d8a83cc
Emily McNeil <Emily@DESKTOP-L16AD2B>
2017-04-13 16:48:49 -0400

    Fixed Story indentations

--- src/edu/udel/cisc275/section011/team0/EstuaryGame/Controller/ShoreController.java
+++ src/edu/udel/cisc275/section011/team0/EstuaryGame/Controller/ShoreController.java
@@ -1,15 +1,25 @@
 package edu.udel.cisc275.section011.team0.EstuaryGame.Controller;
 
+import java.awt.event.MouseAdapter;
+
+import javax.swing.JComponent;
+
 import edu.udel.cisc275.section011.team0.EstuaryGame.Model.ShoreModel;
 import edu.udel.cisc275.section011.team0.EstuaryGame.View.ShoreView;
 
-public class ShoreController {
+public class ShoreController extends MouseAdapter implements Controller {
 
 	private ShoreModel model;
 	private ShoreView view;
 	
+	@Override
 	public void tick(){
 		return;
+	}
+
+	@Override
+	public JComponent getView() {
+		return view;
 	}
 	
 	
--- src/edu/udel/cisc275/section011/team0/EstuaryGame/Model/StoryCubeFace.java
+++ src/edu/udel/cisc275/section011/team0/EstuaryGame/Model/StoryCubeFace.java
@@ -1,12 +1,13 @@
 package edu.udel.cisc275.section011.team0.EstuaryGame.Model;
 
 public class StoryCubeFace {
-private final String type;
+	private final String type;
 
-public String getType() {
-	return type;
-}
-public StoryCubeFace (String type){
-this.type = type;
-}
+	public String getType() {
+		return type;
+	}
+
+	public StoryCubeFace (String type){
+		this.type = type;
+	}
 }
