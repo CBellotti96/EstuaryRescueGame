9fac76043a6e0a9a61b25c396dd9afef6fd1706a
Emily McNeil <Emily@DESKTOP-L16AD2B>
2017-04-13 16:18:44 -0400

    Added menu components

---
+++ src/edu/udel/cisc275/section011/team0/EstuaryGame/Model/StoryMenuItem.java
@@ -0,0 +1,16 @@
+package edu.udel.cisc275.section011.team0.EstuaryGame.Model;
+
+public class StoryMenuItem implements MenuItem {
+
+	@Override
+	public String getName() {
+		return "Story Cubes";
+	}
+
+	@Override
+	public void onClick() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
