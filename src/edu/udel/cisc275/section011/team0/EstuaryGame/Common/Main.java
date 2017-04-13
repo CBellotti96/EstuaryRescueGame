46cea3d92a5cca2ab5bcbad643c443089484172f
Alexandra Hurst <ahurst@udel.edu>
2017-04-13 17:01:35 -0400

    Modify Main to serve as a Controller switcher

--- src/edu/udel/cisc275/section011/team0/EstuaryGame/Common/Main.java
+++ src/edu/udel/cisc275/section011/team0/EstuaryGame/Common/Main.java
@@ -1,9 +1,60 @@
 package edu.udel.cisc275.section011.team0.EstuaryGame.Common;
 
-public class Main {
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-	public static void main (String args[]) {
-		System.out.println("placeholder");
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+import edu.udel.cisc275.section011.team0.EstuaryGame.Controller.MenuController;
+
+public class Main extends JFrame implements ActionListener {
+
+	private static Main instance;
+	
+	public Main getInstance() {
+		return instance;
 	}
 	
+	private Controller currentController;
+
+	private Main() {
+		setBackground(Color.gray);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setSize(View.FRAME_WIDTH, View.FRAME_HEIGHT);
+		setVisible(true);
+	}
+
+	public static void main (String args[]) {
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				instance = new Main();
+
+				Timer timer = new Timer(75, instance);
+				timer.setActionCommand("Tick");
+				timer.setRepeats(true);
+				timer.start();
+				
+				setController(new MenuController());
+			}
+		});
+	}
+
+	public void setController (Controller controller) {
+		currentController = controller;
+		getContentPane().removeAll();
+		getContentPane().add(controller.getView());
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if ("Tick".equals(e.getActionCommand())) {
+			currentController.tick();
+		}
+	}
+
+
+
 }
