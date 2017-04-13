259032c44f5ab4befe44a20a8c552a6a9226f25e
Alexandra Hurst <ahurst@udel.edu>
2017-04-13 11:56:14 -0400

    Added some preliminary variables for the Maze game and made MazeController extend KeyAdapter to allow keyboard input

--- src/edu/udel/cisc275/section011/team0/EstuaryGame/Controller/MazeController.java
+++ src/edu/udel/cisc275/section011/team0/EstuaryGame/Controller/MazeController.java
@@ -1,14 +1,36 @@
 package edu.udel.cisc275.section011.team0.EstuaryGame.Controller;
 
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 import edu.udel.cisc275.section011.team0.EstuaryGame.Model.MazeModel;
 import edu.udel.cisc275.section011.team0.EstuaryGame.View.MazeView;
 
-public class MazeController {
+public class MazeController extends KeyAdapter {
 
 	private MazeModel model;
 	private MazeView view;
 	
+	public MazeController() {
+		model = new MazeModel();
+		view = new MazeView();
+		view.addKeyListener(this);
+	}
+	
 	public void tick(){
 		return;
 	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
--- src/edu/udel/cisc275/section011/team0/EstuaryGame/View/MazeView.java
+++ src/edu/udel/cisc275/section011/team0/EstuaryGame/View/MazeView.java
@@ -1,12 +1,34 @@
 package edu.udel.cisc275.section011.team0.EstuaryGame.View;
 
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
 import javax.swing.JComponent;
 
 public class MazeView extends JComponent {
 	
+	private Rectangle salineGuageRect;
+	private BufferedImage salineGaugeImg[];
+	
+	private Rectangle weatherIconRect;
+	private BufferedImage sunWeatherIconImg;
+	private BufferedImage rainWeatherIconImg;
+	
+	private Rectangle timerRect;
+	
+	private Rectangle miniMapRect;
+	private BufferedImage mimiMapImg;
+	
+	private BufferedImage crabImg;
+	private BufferedImage predatorImg[];
+	private BufferedImage obstacleImage[];
+	
+	private BufferedImage sandTileImg;
+	private BufferedImage waterTileImg;
 	
 	public void paint(Graphics g){
 		return;
 	}
+	
 }
