a780ca117d7c4b0147b3b46eb4285e6807161455
Emily McNeil <Emily@DESKTOP-L16AD2B>
2017-04-14 13:55:33 -0400

    MenuModel constructor added

--- src/edu/udel/cisc275/section011/team0/EstuaryGame/Model/MenuModel.java
+++ src/edu/udel/cisc275/section011/team0/EstuaryGame/Model/MenuModel.java
@@ -1,12 +1,16 @@
 package edu.udel.cisc275.section011.team0.EstuaryGame.Model;
 
-import java.awt.MenuItem;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MenuModel {
 	List<MenuItem> menuItems = new ArrayList<>();
-
 	
+	
+	public MenuModel(){
+		menuItems.add(new MazeMenuItem());
+		menuItems.add(new ShoreMenuItem());
+		menuItems.add(new StoryMenuItem());
+	}
 
 }
