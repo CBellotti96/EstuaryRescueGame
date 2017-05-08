bc6356451871e4c9d37f35566bf51844c11886c3
Alexandra Hurst <ahurst@udel.edu>
2017-05-08 03:26:49 -0400

    Created sanbar images and changed wall rendering to use sandbar images.

--- src/edu/udel/cisc275/section011/team0/EstuaryGame/View/MazeView.java
+++ src/edu/udel/cisc275/section011/team0/EstuaryGame/View/MazeView.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 
+import edu.udel.cisc275.section011.team0.EstuaryGame.Model.Direction;
 import edu.udel.cisc275.section011.team0.EstuaryGame.Model.MazeCrab;
 import edu.udel.cisc275.section011.team0.EstuaryGame.Model.MazeModel;
 import edu.udel.cisc275.section011.team0.EstuaryGame.Model.MazeSection;
@@ -37,7 +38,13 @@ public class MazeView extends JComponent {
 	private BufferedImage predatorImg[];
 	private BufferedImage obstacleImage[];
 	
-	private BufferedImage sandBarImg;
+	private BufferedImage sandbarImg[];
+	private final int SANDBAR_IMG_COUNT = Direction.NORTH.getFlag() 
+			+ Direction.EAST.getFlag()
+			+ Direction.SOUTH.getFlag()
+			+ Direction.WEST.getFlag() + 1;
+	private final int SANDBAR_IMG_SIZE = 64;
+	
 	private BufferedImage waterTileImg[];
 	private double waterTileImgIndex = 0;
 	private final int WATER_TILE_IMG_FRAME_COUNT = 15;
@@ -58,6 +65,56 @@ public class MazeView extends JComponent {
 				waterTileImg[i] = waterTileImgFull.getSubimage(i * WATER_TILE_IMG_FRAME_SIZE, 0, 
 						WATER_TILE_IMG_FRAME_SIZE, WATER_TILE_IMG_FRAME_SIZE);
 			}
+			
+			sandbarImg = new BufferedImage[SANDBAR_IMG_COUNT];
+			sandbarImg[Direction.NORTH.getFlag() 
+			           | Direction.EAST.getFlag() 
+			           | Direction.SOUTH.getFlag()
+			           | Direction.WEST.getFlag()] 
+			        		   = ImageIO.read(new File("Final Images/Objects/sandbar_north_east_south_west.png"));
+			sandbarImg[Direction.NORTH.getFlag() 
+			           | Direction.EAST.getFlag() 
+			           | Direction.SOUTH.getFlag()] 
+			        		   = ImageIO.read(new File("Final Images/Objects/sandbar_north_east_south.png"));
+			sandbarImg[Direction.EAST.getFlag() 
+			           | Direction.SOUTH.getFlag()
+			           | Direction.WEST.getFlag()] 
+			        		   = ImageIO.read(new File("Final Images/Objects/sandbar_east_south_west.png"));
+			sandbarImg[Direction.NORTH.getFlag() 
+			           | Direction.SOUTH.getFlag()
+			           | Direction.WEST.getFlag()] 
+			        		   = ImageIO.read(new File("Final Images/Objects/sandbar_north_south_west.png"));
+			sandbarImg[Direction.NORTH.getFlag() 
+			           | Direction.EAST.getFlag() 
+			           | Direction.WEST.getFlag()] 
+			        		   = ImageIO.read(new File("Final Images/Objects/sandbar_north_east_west.png"));
+			sandbarImg[Direction.NORTH.getFlag() 
+			           | Direction.SOUTH.getFlag()] 
+			        		   = ImageIO.read(new File("Final Images/Objects/sandbar_north_south.png"));
+			sandbarImg[Direction.EAST.getFlag() 
+			           | Direction.WEST.getFlag()] 
+			        		   = ImageIO.read(new File("Final Images/Objects/sandbar_east_west.png"));
+			sandbarImg[Direction.NORTH.getFlag() 
+			           | Direction.EAST.getFlag()] 
+			        		   = ImageIO.read(new File("Final Images/Objects/sandbar_north_east.png"));
+			sandbarImg[Direction.EAST.getFlag() 
+			           | Direction.SOUTH.getFlag()] 
+			        		   = ImageIO.read(new File("Final Images/Objects/sandbar_east_south.png"));
+			sandbarImg[Direction.SOUTH.getFlag()
+			           | Direction.WEST.getFlag()] 
+			        		   = ImageIO.read(new File("Final Images/Objects/sandbar_south_west.png"));
+			sandbarImg[Direction.NORTH.getFlag() 
+			           | Direction.WEST.getFlag()] 
+			        		   = ImageIO.read(new File("Final Images/Objects/sandbar_north_west.png"));
+			sandbarImg[Direction.NORTH.getFlag()] 
+			        		   = ImageIO.read(new File("Final Images/Objects/sandbar_north.png"));
+			sandbarImg[Direction.EAST.getFlag()] 
+			        		   = ImageIO.read(new File("Final Images/Objects/sandbar_east.png"));
+			sandbarImg[Direction.SOUTH.getFlag()] 
+			        		   = ImageIO.read(new File("Final Images/Objects/sandbar_south.png"));
+			sandbarImg[Direction.WEST.getFlag()] 
+	        		   = ImageIO.read(new File("Final Images/Objects/sandbar_west.png"));
+			sandbarImg[0] = ImageIO.read(new File("Final Images/Objects/sandbar_none.png"));
 			
 		} catch(IOException e) {			
 			e.printStackTrace();
@@ -86,63 +143,47 @@ public class MazeView extends JComponent {
 		final int PLAYER_SIZE = TILE_SIZE / 2;
 		final int centerOffsetX = screenWidth / 2 - PLAYER_SIZE / 2;
 		final int centerOffsetY = screenHeight / 2 - PLAYER_SIZE / 2;
-		for (int x = 0, i = 0; x < screenWidth + WATER_TILE_IMG_FRAME_SIZE; x += WATER_TILE_IMG_FRAME_SIZE, i++) {
-			for (int y = 0, j = 0; y < screenHeight + WATER_TILE_IMG_FRAME_SIZE; y += WATER_TILE_IMG_FRAME_SIZE, j++) {
-				
-				//g.drawRect(x, y, unit, unit);
-				//g.fillRect(x, y, unit, unit);
+		final int WATER_TILE_SIZE = Math.max(TILE_SIZE, WATER_TILE_IMG_FRAME_SIZE);
+		for (int x = 0, i = 0; x < screenWidth + WATER_TILE_SIZE; x += WATER_TILE_SIZE, i++) {
+			for (int y = 0, j = 0; y < screenHeight + WATER_TILE_SIZE; y += WATER_TILE_SIZE, j++) {
 				g.drawImage(waterTileImg[(int) waterTileImgIndex], x, y,
-						WATER_TILE_IMG_FRAME_SIZE, WATER_TILE_IMG_FRAME_SIZE, null);
+						WATER_TILE_SIZE, WATER_TILE_SIZE, null);
 			}
 		}
 		waterTileImgIndex = (waterTileImgIndex + 0.05) % WATER_TILE_IMG_FRAME_COUNT;
-		//g.setColor(Color.BLUE);
-		//g.drawRect(0, 0, screenWidth, screenHeight);
-		//g.fillRect(0, 0, screenWidth, screenHeight);
+
 		MazeSection section = model.getCurrentSection();
-		g.setColor(Color.ORANGE);
-		for (int y = 0; y < section.getHeight(); y++) {
-			for (int x = 0; x < section.getWidth(); x++) {
-				if ((section.getCell(y, x) & MazeSection.W) == 0) {
-					g.drawLine((int) ((x - player.getXPos()) * TILE_SIZE) + centerOffsetX,
-							(int) ((y - player.getYPos()) * TILE_SIZE) + centerOffsetY, 
-							(int) ((x - player.getXPos()) * TILE_SIZE) + centerOffsetX, 
-							(int) ((y - player.getYPos()) * TILE_SIZE) + centerOffsetY + TILE_SIZE);
+		for (int y = 0; y <= section.getHeight(); y++) {
+			for (int x = 0; x <= section.getWidth(); x++) {
+				int directions = 0;
+				if (y > 0 && (x > 0 && (section.getCell(y - 1, x - 1) & MazeSection.E) == 0
+						|| x < section.getWidth() && (section.getCell(y - 1, x) & MazeSection.W) == 0)) {
+					directions += Direction.NORTH.getFlag();
 				}
-				if ((section.getCell(y, x) & MazeSection.E) == 0) {
-					g.drawLine((int) ((x - player.getXPos()) * TILE_SIZE) + centerOffsetX + TILE_SIZE,
-							(int) ((y - player.getYPos()) * TILE_SIZE) + centerOffsetY, 
-							(int) ((x - player.getXPos()) * TILE_SIZE) + centerOffsetX + TILE_SIZE, 
-							(int) ((y - player.getYPos()) * TILE_SIZE) + centerOffsetY + TILE_SIZE);
+				if (x > 0 && (y > 0 && (section.getCell(y - 1, x - 1) & MazeSection.S) == 0
+						|| y < section.getHeight() && (section.getCell(y, x - 1) & MazeSection.N) == 0)) {
+					directions += Direction.WEST.getFlag();
 				}
-				if ((section.getCell(y, x) & MazeSection.N) == 0) {
-					g.drawLine((int) ((x - player.getXPos()) * TILE_SIZE) + centerOffsetX,
-							(int) ((y - player.getYPos()) * TILE_SIZE) + centerOffsetY, 
-							(int) ((x - player.getXPos()) * TILE_SIZE) + centerOffsetX + TILE_SIZE, 
-							(int) ((y - player.getYPos()) * TILE_SIZE) + centerOffsetY);
+				if (y < section.getHeight() && (x > 0 && (section.getCell(y, x - 1) & MazeSection.E) == 0
+						|| x < section.getWidth() && (section.getCell(y, x) & MazeSection.W) == 0)) {
+					directions += Direction.SOUTH.getFlag();
 				}
-				if ((section.getCell(y, x) & MazeSection.S) == 0) {
-					g.drawLine((int) ((x - player.getXPos()) * TILE_SIZE) + centerOffsetX,
-							(int) ((y - player.getYPos()) * TILE_SIZE) + centerOffsetY + TILE_SIZE, 
-							(int) ((x - player.getXPos()) * TILE_SIZE) + centerOffsetX + TILE_SIZE, 
-							(int) ((y - player.getYPos()) * TILE_SIZE) + centerOffsetY + TILE_SIZE);
+				if (x < section.getWidth() && (y > 0 && (section.getCell(y - 1, x) & MazeSection.S) == 0
+						|| y < section.getHeight() && (section.getCell(y, x) & MazeSection.N) == 0)) {
+					directions += Direction.EAST.getFlag();
 				}
-				/*g.setColor(Color.GREEN);
-				if ((section.getCell(y, x) & MazeSection.EXIT) != 0) {
-					g.fillRect((int) ((x - player.getXPos()) * TILE_SIZE) + centerOffsetX,
-							(int) ((y - player.getYPos()) * TILE_SIZE) + centerOffsetY, 
-							TILE_SIZE, 
-							TILE_SIZE);
+				
+				// don't draw walls outside frame
+				final int TILE_X = (int) ((x - player.getXPos()) * TILE_SIZE) + centerOffsetX - TILE_SIZE / 2;
+				final int TILE_Y = (int) ((y - player.getYPos()) * TILE_SIZE) + centerOffsetY - TILE_SIZE / 2;
+				if (TILE_X > 0 - TILE_SIZE
+						&& TILE_X < screenWidth
+						&& TILE_Y > 0 - TILE_SIZE
+						&& TILE_Y < screenHeight) {
+					g.drawImage(sandbarImg[directions], 
+							TILE_X, TILE_Y, TILE_SIZE, TILE_SIZE, null);
 				}
-				g.setColor(Color.PINK);
-				if ((section.getCell(y, x) & MazeSection.ENTRANCE) != 0) {
-					g.fillRect((int) ((x - player.getXPos()) * TILE_SIZE) + centerOffsetX,
-							(int) ((y - player.getYPos()) * TILE_SIZE) + centerOffsetY, 
-							TILE_SIZE, 
-							TILE_SIZE);
-				}*/
 			}
-			
 		}
 		
 		// draw player
