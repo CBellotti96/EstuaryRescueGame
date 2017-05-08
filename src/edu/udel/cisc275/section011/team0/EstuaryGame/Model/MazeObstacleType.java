6886337b09cd10b49386c8ba084545b94034de2a
Emily McNeil <mcneilea@udel.edu>
2017-05-07 23:03:49 -0400

    Added MazeDifficulty enum, implemented predator/[] and obstacle/[] creation in MazeSection. Fixed spelling mistake in MazeCrab method, added MazeObstacleType randomizer method

--- src/edu/udel/cisc275/section011/team0/EstuaryGame/Model/MazeObstacleType.java
+++ src/edu/udel/cisc275/section011/team0/EstuaryGame/Model/MazeObstacleType.java
@@ -1,5 +1,10 @@
 package edu.udel.cisc275.section011.team0.EstuaryGame.Model;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public enum MazeObstacleType {
 	TRASH(0.07, 0.6, "trash"),
 	SEAWEED(0.0, 0.3, "seaweed");
@@ -7,6 +12,11 @@ public enum MazeObstacleType {
 	private final double defaultSpeed;
 	private final double interferenceFactor;
 	private final String obstacleType;
+
+	//Reference: http://stackoverflow.com/a/1972399
+	private static final List<MazeObstacleType> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
+	private static final int SIZE = VALUES.size();
+	private static final Random RANDOM = new Random();
 	
 	MazeObstacleType(double defaultSpeed, double interferenceFactor, String obstacleType){
 		this.defaultSpeed = defaultSpeed;
@@ -16,6 +26,13 @@ public enum MazeObstacleType {
 	
 	public String toString(){
 		return this.obstacleType;
+	}
+	
+	/* Generating random selection from enum values
+	Solution Referenced: http://stackoverflow.com/a/1972399
+	*/
+	public static MazeObstacleType randomMazeObstacleType()  {
+		return VALUES.get(RANDOM.nextInt(SIZE));
 	}
 	
 	public double getDefaultSpeed(){
