12ffba47aabfae90416007827ce8832222f767c0
Emily McNeil <mcneilea@udel.edu>
2017-05-10 21:29:32 -0400

    Javadocs for MenuItem, MazeMenuItem

--- src/edu/udel/cisc275/section011/team0/EstuaryGame/Model/MazeMenuItem.java
+++ src/edu/udel/cisc275/section011/team0/EstuaryGame/Model/MazeMenuItem.java
@@ -3,13 +3,25 @@ package edu.udel.cisc275.section011.team0.EstuaryGame.Model;
 import edu.udel.cisc275.section011.team0.EstuaryGame.Common.Main;
 import edu.udel.cisc275.section011.team0.EstuaryGame.Controller.MazeController;
 
+/**
+ * @author Emily McNeil
+ * @author Alexandra Hurst
+ * 
+ * MazeMenuItem creates the model for a menu button leading to a new Maze game instance.
+ */
 public class MazeMenuItem implements MenuItem {
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getName() {
 		return "Maze";
 	}
 
+	/**
+	 * {@inheritDoc}}
+	 */
 	@Override
 	public void onClick() {
 		Main.getInstance().setController(new MazeController());
--- src/edu/udel/cisc275/section011/team0/EstuaryGame/Model/MenuItem.java
+++ src/edu/udel/cisc275/section011/team0/EstuaryGame/Model/MenuItem.java
@@ -1,7 +1,20 @@
 package edu.udel.cisc275.section011.team0.EstuaryGame.Model;
 
+/**
+ * @author Alexandra Hurst
+ * 
+ * MenuItem interface is used to streamline creation of button models for the menu.
+ */
 public interface MenuItem {
+	/**
+	 * Getter for the name of the button.
+	 * @return a string specifying the prefix name of the invoked Controller class.
+	 */
 	String getName();
+	
+	/**
+	 * Creates a new instance of the specified Controller when MenuItem is clicked.
+	 */
 	void onClick();
 	
 }
