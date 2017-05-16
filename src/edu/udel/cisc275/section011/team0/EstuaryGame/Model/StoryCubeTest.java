d97e1884f3ffdeda6e751e5c0d1c8f21a66fa9b7
bwiswell <bwiswell@udel.edu>
2017-05-15 23:06:49 -0400

    Adding tests to StoryCube

--- src/edu/udel/cisc275/section011/team0/EstuaryGame/Model/StoryCubeTest.java
+++ src/edu/udel/cisc275/section011/team0/EstuaryGame/Model/StoryCubeTest.java
@@ -2,6 +2,7 @@ package edu.udel.cisc275.section011.team0.EstuaryGame.Model;
 
 import static org.junit.Assert.*;
 
+import java.awt.Point;
 import java.awt.Rectangle;
 
 import org.junit.Test;
@@ -14,7 +15,7 @@ public class StoryCubeTest {
 		int n = 86;
 		for (int i = 0; i < n; i++)
 			sc.incrementRoll();
-		assertEquals(n % 10, sc.getRollState());
+		assertEquals(n, sc.getRollState());
 	}
 
 	@Test
@@ -27,17 +28,167 @@ public class StoryCubeTest {
 		int scaledSize = (int) scale * StoryCube.size;
 		double halfSize = scaledSize / 2;
 		Rectangle expected = new Rectangle((int) (x * scale - halfSize) + xMargin, (int) (y * scale - halfSize), scaledSize, scaledSize);
-		System.out.println(expected);
 		sc.setX(30);
 		sc.setY(20);
 		Rectangle actual = sc.getRect(scale, xMargin);
-		System.out.println(actual);
 		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void testMove() {
-		fail("Not yet implemented");
+	public void testMoveSnapIntoPlace() {
+		StoryCube sc = new StoryCube(0);
+		int targetX = sc.getCubePos().getX();
+		int targetY = sc.getCubePos().getY();
+		int x = targetX - StoryCube.speed;
+		int y = targetY - StoryCube.speed;
+		sc.setX(x);
+		sc.setY(y);
+		sc.move();
+		int newX = (int) sc.getRect(1, 0).getCenterX();
+		int newY = (int) sc.getRect(1, 0).getCenterY();
+		assertEquals(targetX, newX);
+		assertEquals(targetY, newY);
+	}
+	
+	@Test
+	public void testMoveSnapXAbove() {
+		StoryCube sc = new StoryCube(0);
+		int targetX = sc.getCubePos().getX();
+		int targetY = sc.getCubePos().getY();
+		int x = targetX - StoryCube.speed;
+		int y = targetY - 3 * StoryCube.speed;
+		sc.setX(x);
+		sc.setY(y);
+		sc.move();
+		int newX = (int) sc.getRect(1, 0).getCenterX();
+		int newY = (int) sc.getRect(1, 0).getCenterY();
+		assertEquals(targetX, newX);
+		assertEquals(y + StoryCube.speed, newY);
+	}
+	
+	@Test
+	public void testMoveSnapXBelow() {
+		StoryCube sc = new StoryCube(0);
+		int targetX = sc.getCubePos().getX();
+		int targetY = sc.getCubePos().getY();
+		int x = targetX - StoryCube.speed;
+		int y = targetY + 3 * StoryCube.speed;
+		sc.setX(x);
+		sc.setY(y);
+		sc.move();
+		int newX = (int) sc.getRect(1, 0).getCenterX();
+		int newY = (int) sc.getRect(1, 0).getCenterY();
+		assertEquals(targetX, newX);
+		assertEquals(y - StoryCube.speed, newY);
+	}
+	
+	@Test
+	public void testMoveSnapYLeft() {
+		StoryCube sc = new StoryCube(0);
+		int targetX = sc.getCubePos().getX();
+		int targetY = sc.getCubePos().getY();
+		int x = targetX - 3 * StoryCube.speed;
+		int y = targetY - StoryCube.speed;
+		sc.setX(x);
+		sc.setY(y);
+		sc.move();
+		int newX = (int) sc.getRect(1, 0).getCenterX();
+		int newY = (int) sc.getRect(1, 0).getCenterY();
+		assertEquals(x + StoryCube.speed, newX);
+		assertEquals(targetY, newY);
+	}
+	
+	@Test
+	public void testMoveSnapYRight() {
+		StoryCube sc = new StoryCube(0);
+		int targetX = sc.getCubePos().getX();
+		int targetY = sc.getCubePos().getY();
+		int x = targetX + 3 * StoryCube.speed;
+		int y = targetY - StoryCube.speed;
+		sc.setX(x);
+		sc.setY(y);
+		sc.move();
+		int newX = (int) sc.getRect(1, 0).getCenterX();
+		int newY = (int) sc.getRect(1, 0).getCenterY();
+		assertEquals(x - StoryCube.speed, newX);
+		assertEquals(targetY, newY);
+	}
+	
+	@Test
+	public void testMoveSE() {
+		StoryCube sc = new StoryCube(0);
+		int targetX = sc.getCubePos().getX();
+		int targetY = sc.getCubePos().getY();
+		int x = targetX - 3 * StoryCube.speed;
+		int y = targetY - 3 * StoryCube.speed;
+		sc.setX(x);
+		sc.setY(y);
+		sc.move();
+		int newX = (int) sc.getRect(1, 0).getCenterX();
+		int newY = (int) sc.getRect(1, 0).getCenterY();
+		assertEquals(x + StoryCube.speed, newX);
+		assertEquals(y + StoryCube.speed, newY);
+	}
+	
+	@Test
+	public void testMoveSW() {
+		StoryCube sc = new StoryCube(0);
+		int targetX = sc.getCubePos().getX();
+		int targetY = sc.getCubePos().getY();
+		int x = targetX + 3 * StoryCube.speed;
+		int y = targetY - 3 * StoryCube.speed;
+		sc.setX(x);
+		sc.setY(y);
+		sc.move();
+		int newX = (int) sc.getRect(1, 0).getCenterX();
+		int newY = (int) sc.getRect(1, 0).getCenterY();
+		assertEquals(x - StoryCube.speed, newX);
+		assertEquals(y + StoryCube.speed, newY);
+	}
+	
+	@Test
+	public void testMoveNE() {
+		StoryCube sc = new StoryCube(0);
+		int targetX = sc.getCubePos().getX();
+		int targetY = sc.getCubePos().getY();
+		int x = targetX - 3 * StoryCube.speed;
+		int y = targetY + 3 * StoryCube.speed;
+		sc.setX(x);
+		sc.setY(y);
+		sc.move();
+		int newX = (int) sc.getRect(1, 0).getCenterX();
+		int newY = (int) sc.getRect(1, 0).getCenterY();
+		assertEquals(x + StoryCube.speed, newX);
+		assertEquals(y - StoryCube.speed, newY);
+	}
+	
+	@Test
+	public void testMoveNW() {
+		StoryCube sc = new StoryCube(0);
+		int targetX = sc.getCubePos().getX();
+		int targetY = sc.getCubePos().getY();
+		int x = targetX + 3 * StoryCube.speed;
+		int y = targetY + 3 * StoryCube.speed;
+		sc.setX(x);
+		sc.setY(y);
+		sc.move();
+		int newX = (int) sc.getRect(1, 0).getCenterX();
+		int newY = (int) sc.getRect(1, 0).getCenterY();
+		assertEquals(x - StoryCube.speed, newX);
+		assertEquals(y - StoryCube.speed, newY);
+	}
+	
+	@Test
+	public void testMoveSelected() {
+		StoryCube sc = new StoryCube(0);
+		int x = sc.getCubePos().getX();
+		int y = sc.getCubePos().getY();
+		sc.setSelected(true);
+		sc.move();
+		int newX = (int) sc.getRect(1, 0).getCenterX();
+		int newY = (int) sc.getRect(1, 0).getCenterY();
+		assertEquals(x, newX);
+		assertEquals(y, newY);
 	}
 
 }
